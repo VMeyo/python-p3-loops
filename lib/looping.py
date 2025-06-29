11bed3d38ab251e15cbf53fc51ba69d224a5b99a
Victor <victor.meyo@student.moringaschool.com>
2025-06-29 12:22:53 +0300

    Additional comments added

--- lib/looping.py
+++ lib/looping.py
@@ -1,20 +1,22 @@
-#!/usr/bin/env python3
+# Function to print happy new ear after decrementing from 10 to 1
 
 def happy_new_year():
-    # code goes here!
-    count = 10
-    while count > 0:
+    count = 10 # Count should start from 10
+    while count > 0: # Sequence should oroceed as long as count is greater than 0
         print(count)
-        count -= 1
-    print("Happy New Year!")
+        count -= 1 # Decrement from 10 to 1
 
-def square_integers(int_list):
-    return [num ** 2 for num in int_list]
-    # code goes here!
+    print("Happy New Year!") # Print happy new ear after the sequence is done
+
+# Function to print the square of integers
+
+def square_integers(int_list): # Takes in a list as the argument
+    return [num ** 2 for num in int_list] # Use list comprehension to square the numbers
     
+    
+# Function that checks on multiples of 3 & 5
 
 def fizzbuzz():
-    # code goes here!
     for num in range(1, 101):
         if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz")
